1319013d4be47dd1158c9a6e840c455f1f12a277
Cherkas <xcherkas@stuba.sk>
2023-05-09 14:32:37 +0200

    Removed trash from master

--- frontend/app/main.js
+++
@@ -1,7 +0,0 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './static/styles/index.less';
-
-import App from './App.svelte';
-export default new App({ target: document.body });
